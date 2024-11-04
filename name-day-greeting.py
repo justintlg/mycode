53d27d90fd0525e94ba120823ef9d3e762d23c12
justintlg <lashley1830@aim.com>
2024-11-04 23:53:16 +0000

    learning about input function

---
+++ name-day-greeting.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+def greet():
+
+    name = input('What is your name?' )
+
+    day = input('What day of the week is it?')
+
+    print('Hello, ', name, '! Happy, ', day,'!', sep='')
+
+greet()
+
