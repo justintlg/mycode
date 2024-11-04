fd11656b2d0e53b543c30e74c2dda066061132c5
justintlg <lashley1830@aim.com>
2024-11-04 23:29:14 +0000

    learning about input function

---
+++ lab_input/iptaker03.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""Alta3 Research | RZFeeser
+   print() - display data to std out"""
+
+# below is a function containing our code
+def main():
+
+    # pause the program and wait for the user to provide input
+    user_input = input("Please enter an IPv4 IP address:")
+    
+    # display the input back to the user.
+    print("You told me the IPv4 address is: " + user_input)
+    
+    #vendor = input('What is the vendor name of your device?:')
+
+
+
+# this calls main
+main()
+
+
+def sec():
+
+    vendor = input('What is the vendor name of your device?:')
+
+    print('Sooooo the vendor name is:', vendor)
+
+sec()
