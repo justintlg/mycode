569913de8a94172a181292e32e0bf98ec9b90829
justintlg <lashley1830@aim.com>
2024-11-05 23:49:13 +0000

    About complex lists

---
+++ advlist/animal-list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def main():
+    
+    #Listing animals
+    animals = ['Ant','Cat','Dog','Koi','Hog']
+
+    #for loop
+    for arg in animals:
+        print(arg, end=' ')
+
+    #new line at end of loop
+    print()
+
+main()
